3d3be0ab291a73d53bff044fd7b2ad96b8eac54c
hazimgharib21 <hazimgharib21@gmail.com>
2018-08-05 23:56:55 +0800

    innitialize commit

---
+++ OpticalFlowForm/OpticalFlowForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace OpticalFlowForm
+{
+    partial class OpticalFlowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox_Source = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StripMenuItem_Video_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.StripMenuItem_Video_Play = new System.Windows.Forms.ToolStripMenuItem();
+            this.StripMenuItem_Video_Pause = new System.Windows.Forms.ToolStripMenuItem();
+            this.StripMenuItem_Video_Stop = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox_Result = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Source)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1433, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox_Source
+            // 
+            this.pictureBox_Source.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox_Source.Name = "pictureBox_Source";
+            this.pictureBox_Source.Size = new System.Drawing.Size(701, 660);
+            this.pictureBox_Source.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_Source.TabIndex = 1;
+            this.pictureBox_Source.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cameraToolStripMenuItem,
+            this.videoToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // cameraToolStripMenuItem
+            // 
+            this.cameraToolStripMenuItem.Name = "cameraToolStripMenuItem";
+            this.cameraToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cameraToolStripMenuItem.Text = "Camera";
+            // 
+            // videoToolStripMenuItem
+            // 
+            this.videoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StripMenuItem_Video_Open,
+            this.StripMenuItem_Video_Play,
+            this.StripMenuItem_Video_Pause,
+            this.StripMenuItem_Video_Stop});
+            this.videoToolStripMenuItem.Name = "videoToolStripMenuItem";
+            this.videoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.videoToolStripMenuItem.Text = "Video";
+            // 
+            // StripMenuItem_Video_Open
+            // 
+            this.StripMenuItem_Video_Open.Name = "StripMenuItem_Video_Open";
+            this.StripMenuItem_Video_Open.Size = new System.Drawing.Size(152, 22);
+            this.StripMenuItem_Video_Open.Text = "Open";
+            this.StripMenuItem_Video_Open.Click += new System.EventHandler(this.StripMenuItem_Video_Open_Click);
+            // 
+            // StripMenuItem_Video_Play
+            // 
+            this.StripMenuItem_Video_Play.Name = "StripMenuItem_Video_Play";
+            this.StripMenuItem_Video_Play.Size = new System.Drawing.Size(152, 22);
+            this.StripMenuItem_Video_Play.Text = "Play";
+            this.StripMenuItem_Video_Play.Click += new System.EventHandler(this.StripMenuItem_Video_Play_Click);
+            // 
+            // StripMenuItem_Video_Pause
+            // 
+            this.StripMenuItem_Video_Pause.Name = "StripMenuItem_Video_Pause";
+            this.StripMenuItem_Video_Pause.Size = new System.Drawing.Size(152, 22);
+            this.StripMenuItem_Video_Pause.Text = "Pause";
+            this.StripMenuItem_Video_Pause.Click += new System.EventHandler(this.StripMenuItem_Video_Pause_Click);
+            // 
+            // StripMenuItem_Video_Stop
+            // 
+            this.StripMenuItem_Video_Stop.Name = "StripMenuItem_Video_Stop";
+            this.StripMenuItem_Video_Stop.Size = new System.Drawing.Size(152, 22);
+            this.StripMenuItem_Video_Stop.Text = "Stop";
+            this.StripMenuItem_Video_Stop.Click += new System.EventHandler(this.StripMenuItem_Video_Stop_Click);
+            // 
+            // pictureBox_Result
+            // 
+            this.pictureBox_Result.Location = new System.Drawing.Point(719, 45);
+            this.pictureBox_Result.Name = "pictureBox_Result";
+            this.pictureBox_Result.Size = new System.Drawing.Size(701, 660);
+            this.pictureBox_Result.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_Result.TabIndex = 2;
+            this.pictureBox_Result.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(677, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // OpticalFlowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1433, 717);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_Result);
+            this.Controls.Add(this.pictureBox_Source);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "OpticalFlowForm";
+            this.Text = "Optical Flow";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Source)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem videoToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox_Source;
+        private System.Windows.Forms.ToolStripMenuItem StripMenuItem_Video_Open;
+        private System.Windows.Forms.ToolStripMenuItem StripMenuItem_Video_Play;
+        private System.Windows.Forms.ToolStripMenuItem StripMenuItem_Video_Pause;
+        private System.Windows.Forms.ToolStripMenuItem StripMenuItem_Video_Stop;
+        private System.Windows.Forms.PictureBox pictureBox_Result;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OpticalFlowForm/OpticalFlowForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace OpticalFlowForm
+{
+    public partial class OpticalFlowForm : Form
+    {
+        VideoCapture capture;
+        bool Pause = false;
+        Image<Gray, byte> _currentImage;
+        Image<Gray, byte> _lastImage;
+        Image<Gray, float> velx;
+        Image<Gray, float> vely;
+        Image<Bgr, byte> _inputImage;
+        int count = 0;
+
+        public OpticalFlowForm()
+        {
+            InitializeComponent();
+            
+            StripMenuItem_Video_Pause.Enabled = Pause;
+            StripMenuItem_Video_Play.Enabled = false;
+            StripMenuItem_Video_Stop.Enabled = false;
+        }
+
+        private void StripMenuItem_Camera_Open_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StripMenuItem_Video_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                capture = new VideoCapture(ofd.FileName);
+                Mat m = new Mat();
+                capture.Read(m);
+                _inputImage = m.ToImage<Bgr, byte>();
+                Image<Gray, byte> grayImage = m.ToImage<Gray, byte>();
+                _lastImage = grayImage;
+                
+                pictureBox_Source.Image = m.Bitmap;
+                StripMenuItem_Video_Play.Enabled = true;
+            }
+        }
+
+        private async void StripMenuItem_Video_Play_Click(object sender, EventArgs e)
+        {
+            if(capture == null)
+            {
+                MessageBox.Show("Please Load Video File");
+                return;
+            }
+
+            Pause = false;
+            StripMenuItem_Video_Pause.Enabled = !Pause;
+            StripMenuItem_Video_Play.Enabled = false;
+            StripMenuItem_Video_Stop.Enabled = true;
+
+            try
+            {
+                while (!Pause)
+                {
+                    Mat m = new Mat();
+                    capture.Read(m);
+                    _inputImage = m.ToImage<Bgr, byte>();
+
+                    if (!m.IsEmpty)
+                    {
+                        count += 1;
+                        label1.Text = count.ToString();
+                        Image<Gray, byte> grayImage = m.ToImage<Gray, byte>();
+                        _currentImage = grayImage;
+                        pictureBox_Source.Image = m.Bitmap;
+                        ComputeDenseOpticalFlow();
+                        double fps = capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
+                        await Task.Delay(1000 / Convert.ToInt32(fps));
+                        _lastImage = _currentImage;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StripMenuItem_Video_Pause_Click(object sender, EventArgs e)
+        {
+            Pause = true;
+            StripMenuItem_Video_Pause.Enabled = !Pause;
+            StripMenuItem_Video_Play.Enabled = true;
+        }
+
+        private void StripMenuItem_Video_Stop_Click(object sender, EventArgs e)
+        {
+            Pause = true;
+            capture.Stop();
+            pictureBox_Source.Image = null;
+            StripMenuItem_Video_Pause.Enabled = false;
+            StripMenuItem_Video_Play.Enabled = false;
+            StripMenuItem_Video_Stop.Enabled = false;
+        }
+
+        void ComputeDenseOpticalFlow()
+        {
+            velx = new Image<Gray, float>(_lastImage.Size);
+            vely = new Image<Gray, float>(_currentImage.Size);
+            Image<Gray, byte> result = _currentImage.CopyBlank();
+
+            CvInvoke.CalcOpticalFlowFarneback(_lastImage, _currentImage, velx, vely,
+                         0.5, 3, 15, 3, 6, 1.3, 0);
+
+            DrawFarnerBackFlowMap(_inputImage, velx, vely, 1);
+            
+        }
+
+        void DrawFarnerBackFlowMap(Image<Bgr, Byte> img_curr, Image<Gray, float> flow_x, Image<Gray, float> flow_y, int step, int shiftThatCounts = 0)
+        {
+            Point fromDotXY = new Point();
+            Point toDotXY = new Point();
+
+            MCvScalar col;
+            col.V0 = 100;
+            col.V1 = 255;
+            col.V2 = 0;
+            col.V3 = 0;
+
+            for (int i = 0; i < flow_x.Rows; i += step)
+            {
+                for (int j = 0; j < flow_x.Cols; j += step)
+                {
+                    toDotXY.X = (int)flow_x.Data[i, j, 0];
+                    toDotXY.Y = (int)flow_y.Data[i, j, 0];
+
+                    fromDotXY.X = j;
+                    fromDotXY.Y = i;
+
+                    toDotXY.X = fromDotXY.X + toDotXY.X;
+                    toDotXY.Y = fromDotXY.Y + toDotXY.Y;
+
+                    CvInvoke.Line(_inputImage, fromDotXY, toDotXY, new MCvScalar(0, 0, 255), 2, Emgu.CV.CvEnum.LineType.EightConnected, 0);
+
+                    pictureBox_Result.Image = _inputImage.Bitmap;
+                }
+            }
+        }
+    }
+}
